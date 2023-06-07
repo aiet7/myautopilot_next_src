fc8b072e2315999c984e3e9f758c3836088dc4b7
Tim <tim@etech7.com>
2023-06-07 15:56:21 -0400

    Ready for Deployment #6

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
-const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, (err) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(process.env.PORT || 3000, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`);
   });
 });
+
