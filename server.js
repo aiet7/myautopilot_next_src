2c9797a5d1cff3f3810330a03abcf90ef90f78d7
Tim <tim@etech7.com>
2023-06-07 15:23:14 -0400

    Ready for deployment #4

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-
-    handle(req, res, parsedUrl);
-  }).listen(process.env.PORT || 3000, (err) => {
+    handle(req, res);
+  }).listen(port, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
