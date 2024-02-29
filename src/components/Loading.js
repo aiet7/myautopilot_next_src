b16bedbd13825291d9f627d367943168eaa80f9d
Tim <tim@etech7.com>
2024-02-29 12:58:09 -0500

    Trying a different method for deployment.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ const Loading = () => {
   return (
     <div className="bg-white absolute top-0 left-0 right-0 bottom-0 w-full h-full flex flex-col  items-center justify-center gap-4">
       <div className="flex items-end text-8xl loading-dots text-blue-800">
-        <span>.</span>
+        <span></span>
         <span>.</span>
         <span>.</span>
       </div>
