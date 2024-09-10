0e4b2a9e6db3f8cad05d07ee9dc86fd05bc6afcd
Tim <tim@etech7.com>
2024-09-10 15:28:43 -0400

    Again pushing

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+console.log("Server launched!")
 
 app.prepare().then(() => {
   createServer((req, res) => {
