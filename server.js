7a4cfcd485c2c272841de25b608b5ccef832f072
Tim <tim@etech7.com>
2024-09-11 15:35:15 -0400

    Pushing final version of beta

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
+
 require("dotenv").config({ path: ".env" });
 
 const dev = process.env.NODE_ENV !== "production";
-
 const app = next({ dev });
 const handle = app.getRequestHandler();
-console.log("Server launched!")
 
 app.prepare().then(() => {
   createServer((req, res) => {
